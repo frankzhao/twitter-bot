712dbf1ce598c880c235c196ffe183647dab5649
Frank Zhao <frank@frankzhao.net>
2014-06-05 23:57:59 +1000

    db init

---
+++ bot.py
@@ -0,0 +1,46 @@
+# Frank Zhao 2014
+# <frank@frankzhao.net>
+
+import twitter
+import sqlite3
+
+db_connection = sqlite3.connect('db/bot.db') # Connect to db
+db = db_connection.cursor() # Database cursor
+
+# Sets up the initial database
+def init():
+    # Create factoid table
+    db.execute('''
+        CREATE TABLE `factoids` (
+          `id` INTEGER PRIMARY KEY NOT NULL,
+          `kind` TEXT default NULL,
+          `value` TEXT default NULL,
+          `protected` int NOT NULL,
+          `user` TEXT default NULL
+        )''')
+        
+    # Create inventory table
+    db.execute('''
+        CREATE TABLE `inventory` (
+          `id` INTEGER PRIMARY KEY NOT NULL,
+          `value` TEXT default NULL,
+          `user` TEXT default NULL
+        )''')
+        
+    # Create variable types table
+    db.execute('''
+        CREATE TABLE `variables` (
+          `id` INTEGER PRIMARY KEY NOT NULL,
+          `kind` TEXT default NULL,
+          `value` TEXT default NULL,
+          `protected` int NOT NULL
+        )''')
+        
+    # Create variables table
+    db.execute('''
+        CREATE TABLE `vars` (
+          `id` INTEGER PRIMARY KEY NOT NULL,
+          `kind` TEXT default NULL,
+          `value` TEXT default NULL
+        )''')
+    
