3bcc05153ff9057168a7a31be8bdcd65c88c11ea
Frank Zhao <frank@frankzhao.net>
2014-06-06 14:36:00 +1000

    quote error handling

--- bot.py
+++ bot.py
@@ -11,15 +11,21 @@ db = database.Database()
 factoid_count = 0
 inventory_count = 0
 
+def log(msg):
+    print "LOG: " + msg
+
 while True:
     # syntax is "@bot quote @user some quote"
     tweet = raw_input("Enter some text: ")
     # check that the bot has been mentioned
     parsed = re.split(' ', tweet)
-    if (parsed[0] == "@bot" and parsed[1] == "quote"):
-        quotee = re.split(' ', re.findall("quote @[^\s]+", tweet)[0])[1]
-        quote = re.split("@[^\s]+ ", tweet)
-        if len(quote) == 3:
-            quote = re.split("@[^\s]+ ", tweet)[2]
-            db.add_factoid("quote", "quote " + quotee, quote, "quote", 0, "@user")
-    
+    if not len(parsed) < 4:
+        if (parsed[0] == "@bot" and parsed[1] == "quote"):
+            quotee = re.split(' ', re.findall("quote @[^\s]+", tweet)[0])[1]
+            quote = re.split("@[^\s]+ ", tweet)
+            if len(quote) == 3:
+                quote = re.split("@[^\s]+ ", tweet)[2]
+                db.add_factoid("quote", "quote " + quotee, quote, "quote", 0, "@user")
+            else: log("Ignored invalid quote: " + tweet)
+        else: log("Ignored invalid quote: " + tweet)
+    else: log("Ignored invalid quote: " + tweet)
